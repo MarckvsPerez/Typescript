ca9b57a1bdd27869e32e586944512300c43a241b
Marc Pérez <marcantoniperez@gmail.com>
2025-03-20 00:02:19 +0100

    Actualizar index.html para cargar booleans.js y agregar nuevos archivos booleans.js y booleans.ts con lógica de variables booleanas.

---
+++ bases/types/booleans.js
@@ -0,0 +1,7 @@
+"use strict";
+(() => {
+    let isSuperman = false;
+    let isBatman = false;
+    isSuperman = isBatman ? false : true;
+    console.log({ isSuperman });
+})();
---
+++ bases/types/booleans.ts
@@ -0,0 +1,8 @@
+(() => {
+  let isSuperman: boolean = false;
+  let isBatman: boolean = false;
+
+  isSuperman = isBatman ? false : true;
+
+  console.log({ isSuperman });
+})();
