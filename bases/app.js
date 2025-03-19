025fea31843165c38b256f6293c86a436aaf5759
Marc Pérez <marcantoniperez@gmail.com>
2025-03-19 23:53:47 +0100

    Agregar archivos base para el proyecto: app.js, app.ts, index.html, package.json y tsconfig.json. Implementar un script simple que imprime "Hello World" y variables en consola.

---
+++ bases/app.js
@@ -0,0 +1,11 @@
+"use strict";
+(() => {
+    const msg = "Hello World";
+    console.log(msg);
+    // a tiene tipo number
+    const a = 10;
+    console.log(a);
+    // b tiene tipo 10
+    const b = 10;
+    console.log(b);
+})();
---
+++ bases/app.ts
@@ -0,0 +1,12 @@
+(() => {
+  const msg: string = "Hello World";
+  console.log(msg);
+
+  // a tiene tipo number
+  const a: number = 10;
+  console.log(a);
+
+  // b tiene tipo 10
+  const b = 10;
+  console.log(b);
+})();
